a759d283cdf13b38e37319b18ed608478884f890
Alejandro Reyes <alejandro.reyesj@outlook.com>
2024-02-18 11:13:44 -0500

    Starting Shared State

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use bytes::Bytes;
+use mini_redis::{Command, Connection, Frame};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+use tokio::net::{TcpListener, TcpStream};
+
+type SharedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
+
+fn new_sharded_db(num_shards: usize) -> SharedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Started Mini Redis Server...");
+    println!("Listening on port 6379...");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+    loop {
+        let (socket, addr) = listener.accept().await.unwrap();
+        println!("Accepted connection from {addr:?}");
+
+        let db = db.clone();
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    let mut connection = Connection::new(socket);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Command::Set(cmd) => {
+                db.lock()
+                    .unwrap()
+                    .insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Command::Get(cmd) => {
+                if let Some(value) = db.lock().unwrap().get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented: {cmd:?}"),
+        };
+        connection.write_frame(&response).await.unwrap();
+    }
 }
